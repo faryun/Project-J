ad7bd1a61aae4879f959b8d472c59f8cb00e18de
faryun <43097367+faryun@users.noreply.github.com>
2024-03-20 13:54:10 +0900

    주사위 기반 코드

---
+++ Assets/Script/DiceRoll.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DiceRoll : MonoBehaviour
+{
+    public TMP_Text DiceText;
+    public TMP_Text SumText;
+    int[] dice = new int[] {1,2,3,4,5,6};
+    int sum = 0;
+    public void Roll()
+    {
+        int rand = Random.Range(0,6);
+        DiceText.text = dice[rand].ToString();
+        sum = sum + dice[rand];
+        SumText.text = sum.ToString();
+    }
+}
