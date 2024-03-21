c2fba462fae2623522131217e3ad7aa8f0e30c3d
faryun <43097367+faryun@users.noreply.github.com>
2024-03-21 16:43:54 +0900

    Update Dice

--- Assets/Script/DiceList.cs
+++ Assets/Script/DiceList.cs
@@ -1,11 +1,36 @@
+public class DiceEye
+{
+    public int value; // 기본 값
+    public int grade; // 등급 (희귀도)
+    public string effect; // 효과 설명
+    public string[] tags; // 태그 배열
+
+    public DiceEye(int value, int grade, string effect, string[] tags)
+    {
+        this.value = value;
+        this.grade = grade;
+        this.effect = effect;
+        this.tags = tags;
+    }
+}
+
 public class DiceList
 {
-    //기본 주사위
-    public static int[] nomal_dice = new int[] {1,2,3,4,5,6};
-    
-    //홀수 주사위
-    public static int[] odd_dice = new int[] {1,3,5,7,9,11};
+    public static DiceEye[] defaultEyes = new DiceEye[]
+    {
+        new DiceEye(1, 1, "목표값에 1을 더한다.", new string[] {}),
+        new DiceEye(2, 1, "목표값에 2를 더한다.", new string[] {}),
+        new DiceEye(3, 1, "목표값에 3을 더한다.", new string[] {}),
+        new DiceEye(4, 1, "목표값에 4를 더한다.", new string[] {}),
+        new DiceEye(5, 1, "목표값에 5를 더한다.", new string[] {}),
+        new DiceEye(6, 1, "목표값에 6을 더한다.", new string[] {})
+    };
 
-    //짝수 주사위
-    public static int[] even_dice = new int[] {2,4,6,8,10,12};
+    public static DiceEye treasureBox = new DiceEye(1, 2, "목표값에 1을 더한다. 열쇠와 함께 나오면 추가로 10을 더한다.", new string[] { "보물상자" });
+    public static DiceEye key = new DiceEye(1, 2, "목표값에 1을 더한다.", new string[] { "열쇠" });
+    public static DiceEye seven = new DiceEye(1, 2, "목표값에 1을 더한다. 인접한 주사위에 7이 두개 있으면 추가로 74를 더한다.", new string[] { "7" });
+    public static DiceEye hongSalmon = new DiceEye(0, 3, "여고생. 물고기. 버프 '헉'을 얻는다. 헉: 3개 모이면 0으로 초기화되고 목표값에 12를 더한다.", new string[] { "여고생", "물고기", "헉" });
+    public static DiceEye napizak = new DiceEye(2, 3, "여고생. 새. 목표값에 2를 더한다. 홍연어나 오버워치가 인접하면 파괴하고 목표값에 10을 더한다.", new string[] { "여고생", "새" });
+    public static DiceEye overwatch = new DiceEye(0, 3, "파괴되면 목표값에 10을 더한다.", new string[] { "파괴" });
+    public static DiceEye longBird = new DiceEye(0, 3, "새. 이 눈에 모자가 6 이상이라면 목표값에 8, 아니면 2를 더하고 버프 '모자'를 이 눈에 추가한다.", new string[] { "새", "모자" });
 }
--- Assets/Script/DiceRoll.cs
+++ Assets/Script/DiceRoll.cs
@@ -1,25 +1,175 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class DiceRoll : MonoBehaviour
 {
-    bool isClick = false;
+    bool isClick;
     public TMP_Text DiceText;
-    public TMP_Text SumText;
-    public int[] dice = DiceList.nomal_dice;
+    public TMP_Text TargetText;
+    public Image[] diceSlotImages;
+    public int[] diceTypes = new int[] { 0 }; // 기본적으로 0번 주사위만 존재
+    public DiceEye[][] dices = new DiceEye[5][]; // 주사위 눈 저장 배열 (최대 5개)
 
-    int sum = 0;
+    int targetValue = 0; // 목표값
+    List<string> globalBuffs = new List<string>(); // 전역 버프 리스트
+
     public void Roll()
     {
-        int rand = Random.Range(0,6);
-        DiceText.text = dice[rand].ToString();
-        sum = sum + dice[rand];
-        SumText.text = sum.ToString();
+        targetValue = 0; // 새로 굴릴 때마다 목표값 초기화
+        string diceTextValue = "";
+
+        for (int i = 0; i < diceTypes.Length; i++)
+        {
+            int diceType = diceTypes[i];
+            DiceEye rolledEye;
+
+            // 주사위 눈이 비어있으면 기본 1로 설정
+            if (dices[diceType].Length == 0)
+            {
+                rolledEye = DiceList.defaultEyes[0];
+            }
+            else
+            {
+                int rand = Random.Range(0, dices[diceType].Length);
+                rolledEye = dices[diceType][rand];
+            }
+
+            // 주사위 눈 효과 적용
+            ApplyEyeEffect(rolledEye, i);
+
+            diceTextValue += diceType + ":" + rolledEye.value + " ";
+        }
+
+        DiceText.text = diceTextValue;
+        TargetText.text = "Sum: " + targetValue;
+
+        // 주사위 슬롯 UI 업데이트
+        UpdateDiceSlots(); 
+        // 전역 버프 효과 적용
+        ApplyGlobalBuffs();
+    }
+
+    void ApplyEyeEffect(DiceEye eye, int diceIndex)
+    {
+        // 기본 값 적용
+        targetValue += eye.value;
+
+        // 태그별 효과 적용
+        foreach (string tag in eye.tags)
+        {
+            switch (tag)
+            {
+                case "보물상자":
+                    // 열쇠와 함께 나왔는지 확인
+                    bool hasKey = false;
+                    for (int i = 0; i < diceTypes.Length; i++)
+                    {
+                        if (i != diceIndex && dices[i].Contains(DiceList.key))
+                        {
+                            hasKey = true;
+                            break;
+                        }
+                    }
+                    if (hasKey)
+                        targetValue += 10;
+                    break;
+                case "7":
+                    // 인접한 주사위에 7이 두개 있는지 확인
+                    int sevenCount = 0;
+                    for (int i = 0; i < diceTypes.Length; i++)
+                    {
+                        if (i != diceIndex && dices[i].Contains(DiceList.seven))
+                        {
+                            sevenCount++;
+                        }
+                    }
+                    if (sevenCount >= 2)
+                        targetValue += 74;
+                    break;
+                case "헉":
+                    // 전역 버프 '헉' 추가
+                    globalBuffs.Add("헉");
+                    break;
+                // 다른 태그에 대한 효과 추가
+            }
+        }
+    }
+
+    void ApplyGlobalBuffs()
+    {
+        // 전역 버프 '헉' 효과 적용
+        if (globalBuffs.Count >= 3)
+        {
+            targetValue += 12;
+            globalBuffs.Clear();
+        }
+    }
+
+    private int nextDiceType = 1;
+
+    public void AddDiceType()
+    {
+        int newType = nextDiceType;
+        nextDiceType++;
+
+        int[] newDiceTypes = new int[diceTypes.Length + 1];
+        System.Array.Copy(diceTypes, newDiceTypes, diceTypes.Length);
+        newDiceTypes[newDiceTypes.Length - 1] = newType;
+        diceTypes = newDiceTypes;
+        dices[newType] = new DiceEye[0]; // 새로운 주사위 눈 배열 초기화
+
+        // 새로운 주사위에 DefaultEyes 추가
+        for (int i = 0; i < DiceList.defaultEyes.Length; i++)
+        {
+            AddEyeToType(newType, DiceList.defaultEyes[i]);
+        }
+
+        UpdateDiceSlots();
+    }
+
+    public void AddEyeToType(int diceType, DiceEye eye)
+    {
+        // 해당 주사위 종류에 새로운 눈 추가
+        DiceEye[] newEyes = new DiceEye[dices[diceType].Length + 1];
+        System.Array.Copy(dices[diceType], newEyes, dices[diceType].Length);
+        newEyes[newEyes.Length - 1] = eye;
+        dices[diceType] = newEyes;
+    }
+
+    void UpdateDiceSlots()
+    {
+        for (int i = 0; i < diceTypes.Length; i++)
+        {
+            int diceType = diceTypes[i];
+            Image slotImage = diceSlotImages[diceType];
+            
+            if (dices[diceType].Length == 0)
+            {
+                slotImage.sprite = null; // 주사위 눈이 없으면 비움
+            }
+            else
+            {
+                DiceEye eye = dices[diceType][0]; // 첫 번째 주사위 눈 가져옴
+                slotImage.sprite = GetSpriteForEye(eye); // 해당 주사위 눈의 스프라이트 설정
+            }
+        }
+    }
+
+    Sprite GetSpriteForEye(DiceEye eye)
+    {
+        // 여기에 각 주사위 눈에 대한 스프라이트를 가져오는 로직 구현
+        switch (eye.value)
+        {
+            case 1: return Resources.Load<Sprite>("DiceEyes/One");
+            case 2: return Resources.Load<Sprite>("DiceEyes/Two");
+            // ... 다른 눈 값에 대한 스프라이트 로드 추가
+            // 기본 스프라이트 반환
+            default: return Resources.Load<Sprite>("DiceEyes/Default");
+        }
     }
 
     public void ButtonDown()
@@ -32,6 +182,23 @@ public class DiceRoll : MonoBehaviour
     {
         Debug.Log("ButtonUP");
         isClick = false;
+    }
+
+    void Start()
+    {
+        // dices 배열 초기화
+        for (int i = 0; i < dices.Length; i++)
+        {
+            dices[i] = new DiceEye[0]; // 각 인덱스에 빈 배열 할당
+        }
+
+        // 기본 주사위 (0번) 초기화
+        for (int i = 0; i < DiceList.defaultEyes.Length; i++)
+        {
+            AddEyeToType(0, DiceList.defaultEyes[i]);
+        }
+
+        UpdateDiceSlots();
     }
 
     private void Update() 
